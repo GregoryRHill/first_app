318be129ff5ce332faec71f304c6f85f32626f7a
Gregory R. Hill <greg@soulhaven.org>
2013-02-21 00:37:24 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '6376b32bebe7b4bbca3df1269c120ded8eeb4940ed8c51b0d88fe5a2c19956e29c4d8b9ed27ad09f41475523564e1b24bcf81edaf00b28e9b07e4db8060c70a2'
